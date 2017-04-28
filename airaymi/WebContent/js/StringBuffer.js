d88020eb279aa995ec5b7dddafaf3750511ae898
music75 <music75@music75>
2017-04-28 13:00:21 +0900

    수정

--- airaymi/WebContent/js/StringBuffer.js
+++
@@ -1,9 +0,0 @@
-var StringBuffer = function() {
-	this.buffer = new Array();
-}
-StringBuffer.prototype.append = function(obj) {
-	this.buffer.push(obj);
-}
-StringBuffer.prototype.toString = function() {
-	return this.buffer.join("");
-}
